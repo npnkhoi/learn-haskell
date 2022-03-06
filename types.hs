13b951d70624481c7b3e649f20c6afecfe4ebde7
npnkhoi <npnkhoi@gmail.com>
2022-03-06 07:21:44 +0700

    update

--- types.hs
+++ types.hs
@@ -1,14 +1,10 @@
 -- Declarations
-
 anInteger = 2
 aCharacter = 'a'
 aString = "haskell is great"
 aFloat = 1.7
 aBoolean = True
 aList = [1,2..10]
-
--- Operators
-
 
 -- Cool examples
 pythagores = [(a, b, c) | a <- [1..10], b <- [1..10], c <- [1..10], a*a + b*b == c*c, a <= b]
